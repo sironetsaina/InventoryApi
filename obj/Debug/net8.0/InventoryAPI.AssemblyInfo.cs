a4341202d2c4ba4d0d45cb98800e6f184b03e1e1
sironetsaina <ruth.saina@strathmore.edu>
2025-02-14 12:39:34 -0800

    m

--- obj/Debug/net8.0/InventoryAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventoryAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbc8e5f94606394931873b5bf00e976792264f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6658c9487f06c5f4c7ea725da8c9261a77a8f03")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
